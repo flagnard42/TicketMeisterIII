71c40e2692273960d98d2807053c4729cb05e4f8
flagnard42 <banks58@live.marshall.edu>
2016-10-16 15:51:39 -0400

    Add project files.

---
+++ TicketMeisterIII/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketMeisterIII.Startup))]
+namespace TicketMeisterIII
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
